30f935c685c5d6e771475384fd95045a2d7e8d94
Sara <sara.batista.d.felix@accenture.com>
2021-04-21 16:46:29 -0300

    Initial Commit

---
+++ eggplant-brownie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by sara.batista.d.felix on 21/04/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
