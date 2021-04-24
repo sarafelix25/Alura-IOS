5089a68c44a3cab04437952030f7be15b6053be5
Sara <sara.batista.d.felix@accenture.com>
2021-04-24 18:38:44 -0300

    navigation controller

---
+++ eggplant-brownie/Controllers/RefeicoesTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RefeicoesTableViewController.swift
+//  eggplant-brownie
+//
+//  Created by sara.batista.d.felix on 24/04/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import UIKit
+
+
+class RefeicoesTableviewController: UITableViewController {
+ 
+    let refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
+                     Refeicao(nome: "Pizza", felicidade: 4),
+                     Refeicao(nome: "Comida Japonesa", felicidade: 5)]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let refeicao = refeicoes[indexPath.row]
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        celula.textLabel?.text = refeicao.nome
+        
+        return celula
+    }
+}
