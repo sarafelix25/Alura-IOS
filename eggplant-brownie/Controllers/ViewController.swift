b01f34d7e58078d1df03214c53e637d91e014345
Sara <sara.batista.d.felix@accenture.com>
2021-04-24 18:53:15 -0300

    popViewController

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -40,5 +40,7 @@ class ViewController: UIViewController {
         let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
         
         print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        
+        navigationController?.popViewController(animated: <#T##Bool#>)
     }
 }
